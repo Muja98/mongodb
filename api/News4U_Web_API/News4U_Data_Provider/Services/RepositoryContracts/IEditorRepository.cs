880833aba96934dc78177b7bf51cd61b9ef98848
petarTrifunovic98 <peki.trifunovic@gmail.com>
2021-02-04 15:51:26 +0100

    Added register functionality.

--- api/News4U_Web_API/News4U_Data_Provider/Services/RepositoryContracts/IEditorRepository.cs
+++ api/News4U_Web_API/News4U_Data_Provider/Services/RepositoryContracts/IEditorRepository.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using MongoDB.Bson;
+using News4U_Data_Provider.DTOs;
 using News4U_Data_Provider.Entities;
 
 namespace News4U_Data_Provider.Services.RepositoryContracts
 {
     public interface IEditorRepository
     {
-        public Task AddEditor(Editor editor);
-        public Task<Editor> GetEditor(string editorId);
+        public Task AddEditor(EditorRegisterDTO editor);
+        public Task<EditorDTO> GetEditor(string editorId);
+        public Task<bool> StudentExists(string username);
         Task AddNews(string editorId, string newsId);
         Task DeleteNews(string editorId, string newsId);
     }
