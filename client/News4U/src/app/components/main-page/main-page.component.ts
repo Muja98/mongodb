0b09b6fbf02f26eef6c32e46a823d1c87bab3eda
Muja98 <stefan.muja@elfak.rs>
2021-02-07 23:52:39 +0100

    main page finished

--- client/News4U/src/app/components/main-page/main-page.component.ts
+++ client/News4U/src/app/components/main-page/main-page.component.ts
@@ -1,9 +1,13 @@
+import { Router } from '@angular/router';
+import { NewsService } from 'src/app/services/news.service';
 import { Paragraph } from './../../models/paragraph';
 import { News } from './../../models/news';
 import { Component, OnInit, ɵɵclassMapInterpolate1 } from '@angular/core';
 import { trigger, state, transition, style, animate } from '@angular/animations'; 
 import { DOCUMENT } from '@angular/common';
 import { HostListener, Inject } from '@angular/core';
+import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
+import { createConstructor } from 'typescript';
 
 @Component({
   selector: 'app-main-page',
@@ -23,7 +27,17 @@ export class MainPageComponent implements OnInit {
 
   public newsArray: Array<News> = [];
   public news:News;
-  constructor(@Inject(DOCUMENT) document) { }
+  public start:number = 0;
+  public end:number = 6; 
+  public oblastiArray = ["Sport","Hronika","Korona","Vremenska Prognoza"]
+  public tagArray = ["Nis", "Srbija","Vukanja", "Tecin","Tebrica", "Soko Banja", "Leskovac", "Pecenjevce", "Kursumlija", "JSO", "Smoke Mardeljano"]
+  public selectedTagArray = [];
+  public selected:number = -1;
+  public selectedText:string = ""
+  public searchText = "";
+  public selectedTag = -1;
+  public selectedTagText = "";
+  constructor(@Inject(DOCUMENT) document, public service: NewsService, public router:Router) { }
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(e) {
@@ -36,13 +50,88 @@ export class MainPageComponent implements OnInit {
      }
   }
 
+  handleSetSelected(index:number)
+  {
+    this.newsArray = [];
+    this.selected = index;
+    this.start = 0;
+    this.end = 6;
+    
+    let selectedpom;
+    if(index==-1)
+      selectedpom = "";
+    else
+      selectedpom = this.oblastiArray[this.selected]
+    
+    this.selectedText = selectedpom;
+    this.service.getAllNews(this.start,this.end, selectedpom,"",this.selectedTagText).subscribe((el:any)=>{
+      this.newsArray = el;
+   })
+  }
+
+  handleAddClick(index:number)
+  {
+    if(index == this.selectedTag)
+    {
+      this.selectedTagText = ""
+      this.selectedTag=-1;
+    }
+    else
+    {
+      this.selectedTag = index;
+      this.selectedTagText= this.tagArray[index];
+    }
+    
+  }
+
+  handleClickSearch()
+  {
+    this.service.getAllNews(this.start, this.end, this.selectedText, this.searchText,this.selectedTagText).subscribe((el:any)=>{
+      this.newsArray = el;
+   })
+   this.searchText = "";
+
+   this.selectedTagText = ""
+   this.selectedTag=-1;
+  }
+
+  handleCheckNumber(i1, i2)
+  {
+    if((i1%3)===i2) return true;
+    else return false;
+  }
+
+  handleRedirectSpecificNews(id:string)
+  {
+    this.router.navigate(['/specific-news/'+id])
+  }
+
+  handleLoadMore()
+  {
+    this.start = this.end;
+    this.end = this.end+2;
+
+    let selectedpom;
+    if(this.selected==-1)
+      selectedpom = "";
+    else
+      selectedpom = this.oblastiArray[this.selected]
+
+    this.service.getAllNews(this.start,this.end,selectedpom,this.searchText,this.selectedTagText).subscribe((el:any)=>{
+      console.log(el)
+      let pomArray = [];
+      pomArray = el;
+      pomArray.forEach((elpom:any)=>{
+        this.newsArray.push(elpom)
+      })
+   })
+  }
   ngOnInit(): void {
-     this.news = new News();
-     this.news.title = "Velike poplave u Niš-u " ;
+     this.service.getAllNews(this.start,this.end,"","","").subscribe((el:any)=>{
+        this.newsArray = el;
+     })
+ 
      
-     let p1:Paragraph = new Paragraph();
-     p1.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-     this.news.paragraphs.push(p1);
   } 
 
 }
--- client/News4U/src/app/services/news.service.ts
+++ client/News4U/src/app/services/news.service.ts
@@ -17,4 +17,9 @@ export class NewsService {
   getRelatedNews(newsId:string) {
     return this.http.get<News[]>(URL + "/api/news/" + newsId + "/related-news");
   }
+
+  getAllNews(start:number,end:number,field:string,title:string,tag:string)
+  {
+    return this.http.get(URL+"/api/news?from="+start+"&to="+end+"&field="+field+"&title="+title+"&tag="+tag);
+  }
 }
