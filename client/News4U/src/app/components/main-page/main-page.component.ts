b714aa8ce242499984d11d45a702addb4a85a126
Muja98 <stefan.muja@elfak.rs>
2021-02-11 14:17:30 +0100

    main page little edits

--- client/News4U/src/app/components/main-page/main-page.component.ts
+++ client/News4U/src/app/components/main-page/main-page.component.ts
@@ -2,25 +2,14 @@ import { Router } from '@angular/router';
 import { NewsService } from 'src/app/services/news.service';
 import { Paragraph } from './../../models/paragraph';
 import { News } from './../../models/news';
-import { Component, OnInit, ɵɵclassMapInterpolate1 } from '@angular/core';
-import { trigger, state, transition, style, animate } from '@angular/animations'; 
-import { DOCUMENT } from '@angular/common';
-import { HostListener, Inject } from '@angular/core';
-import { faThumbsDown } from '@fortawesome/free-solid-svg-icons';
-import { createConstructor } from 'typescript';
+import { Component, OnInit,  } from '@angular/core';
+
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
-  animations:[ 
-    trigger('fade',
-    [ 
-      state('void', style({ opacity : 0})),
-      transition(':enter',[ animate(300)]),
-      transition(':leave',[ animate(500)]),
-    ]
-)]
+
 })
 export class MainPageComponent implements OnInit {
 
@@ -30,69 +19,24 @@ export class MainPageComponent implements OnInit {
   public start:number = 0;
   public end:number = 6; 
   public oblastiArray = ["Sport","Hronika","Korona","Vremenska Prognoza"]
-  public tagArray = ["Nis", "Srbija","Vukanja", "Tecin","Tebrica", "Soko Banja", "Leskovac", "Pecenjevce", "Kursumlija", "JSO", "Smoke Mardeljano"]
   public selectedTagArray = [];
   public selected:number = -1;
   public selectedText:string = ""
   public searchText = "";
   public selectedTag = -1;
   public selectedTagText = "";
-  constructor(@Inject(DOCUMENT) document, public service: NewsService, public router:Router) { }
+  constructor( public service: NewsService, public router:Router) { }
 
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll(e) {
-     if (window.pageYOffset > 120) {
-       let element = document.getElementById('navbar');
-       element.classList.add('sticky');
-     } else {
-      let element = document.getElementById('navbar');
-        element.classList.remove('sticky'); 
-     }
-  }
 
-  handleSetSelected(index:number)
-  {
-    this.newsArray = [];
-    this.selected = index;
-    this.start = 0;
-    this.end = 6;
-    
-    let selectedpom;
-    if(index==-1)
-      selectedpom = "";
-    else
-      selectedpom = this.oblastiArray[this.selected]
-    
-    this.selectedText = selectedpom;
-    this.service.getAllNews(this.start,this.end, selectedpom,"",this.selectedTagText).subscribe((el:any)=>{
-      this.newsArray = el;
-   })
-  }
-
-  handleAddClick(index:number)
-  {
-    if(index == this.selectedTag)
-    {
-      this.selectedTagText = ""
-      this.selectedTag=-1;
-    }
-    else
-    {
-      this.selectedTag = index;
-      this.selectedTagText= this.tagArray[index];
-    }
-    
-  }
+ 
+ 
 
   handleClickSearch()
   {
-    this.service.getAllNews(this.start, this.end, this.selectedText, this.searchText,this.selectedTagText).subscribe((el:any)=>{
+    this.service.getAllNews(this.start, this.end, this.selectedText, this.searchText,this.selectedTagText).subscribe((el:News[])=>{
       this.newsArray = el;
+     
    })
-   this.searchText = "";
-
-   this.selectedTagText = ""
-   this.selectedTag=-1;
   }
 
   handleCheckNumber(i1, i2)
@@ -117,17 +61,16 @@ export class MainPageComponent implements OnInit {
     else
       selectedpom = this.oblastiArray[this.selected]
 
-    this.service.getAllNews(this.start,this.end,selectedpom,this.searchText,this.selectedTagText).subscribe((el:any)=>{
-      console.log(el)
-      let pomArray = [];
-      pomArray = el;
-      pomArray.forEach((elpom:any)=>{
+    this.service.getAllNews(this.start,this.end,selectedpom,this.searchText,this.selectedTagText).subscribe((el:News[])=>{
+      if(el.length==2) return;
+      el.forEach((elpom:News)=>{
         this.newsArray.push(elpom)
+       
       })
    })
   }
   ngOnInit(): void {
-     this.service.getAllNews(this.start,this.end,"","","").subscribe((el:any)=>{
+     this.service.getAllNews(this.start,this.end,"","","").subscribe((el:News[])=>{
         this.newsArray = el;
      })
  
